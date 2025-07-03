ebbc6501eef589b57ac075c79e9ac4742aa86027
lightbullet92 <naa_1992@mail.ru>
2025-07-03 16:08:17 +0400

    commit

--- SOURCES/SCRIPTS/PHP/_get_copyright.php
+++ SOURCES/SCRIPTS/PHP/_get_copyright.php
@@ -1,3 +1,3 @@
 <?php
-    echo file_get_contents("SOURCES/CONTENT/MAIN/_company.txt") . file_get_contents("SOURCES/CONTENT/MAIN/_copyright.txt") . '2025. Все права защищены.' . PHP_EOL;
+    echo file_get_contents("SOURCES/CONTENT/MAIN/_company.txt") . " " . file_get_contents("SOURCES/CONTENT/MAIN/_copyright.txt") . '2025. Все права защищены.' . PHP_EOL;
 ?>
