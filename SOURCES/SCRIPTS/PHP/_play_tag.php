3d4b7c2da7bc7f962510088195afb2c9884be495
lightbullet92 <naa_1992@mail.ru>
2025-07-08 22:22:03 +0400

    commit

--- SOURCES/SCRIPTS/PHP/_play_tag.php
+++ SOURCES/SCRIPTS/PHP/_play_tag.php
@@ -1,7 +1,10 @@
-<div class="lb_content">
-    <h1>Правила</h1>
-    <p>Расположите костяшки так, чтобы числа шли подряд слева направо и сверху вниз.</p>
-    <div class="puzzle-15" id="divPuzzle-3"></div>
-</div>
-
-<script src="SOURCES/SCRIPTS/GAMES/puzzle15-game.min.js?2"></script>
+<?php
+    echo "\t" . "\t" . "\t" . '<div class="lb_content">' . PHP_EOL;
+//TODO: Load content from "SOURCES/CONTENT/RULES/GAMES/tag.txt"
+    //echo "\t" . "\t" . "\t" . "\t" . '<h1>Правила</h1>' . PHP_EOL;
+    //echo "\t" . "\t" . "\t" . "\t" . '<p>Расположите костяшки так, чтобы числа шли подряд слева направо и сверху вниз.</p>' . PHP_EOL;
+//End of TODO
+    echo "\t" . "\t" . "\t" . "\t" . '<div class="puzzle-15" id="divPuzzle-3"></div>' . PHP_EOL;
+    echo "\t" . "\t" . "\t" . '</div>' . PHP_EOL;
+    echo "\t" . "\t" . "\t" . '<script src="SOURCES/SCRIPTS/GAMES/puzzle15-game.min.js?2"></script>' . PHP_EOL;
+?>
