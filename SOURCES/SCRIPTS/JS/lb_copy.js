197189f64845c6a858ed004336ba771e0dd6248a
lightbullet92 <naa_1992@mail.ru>
2025-08-01 10:56:54 +0400

    commit

---
+++ SOURCES/SCRIPTS/JS/lb_copy.js
@@ -0,0 +1,17 @@
+$(function()
+{
+  function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+  var array=["#LBPaper", "#LBPaperTest", "#LBFabric", "#LBOrigins", "#LBNewPaper", "#LBNewFabric", "#LBSAMP"];
+  array.forEach(function(elem) {
+    $(elem).on("click", function() {
+      copyToClipboard(elem);
+    });
+  });
+});
