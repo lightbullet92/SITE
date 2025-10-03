885239d7d2d99894d0b30ffa6fc1628b9ca1fd78
lightbullet92 <naa_1992@mail.ru>
2025-10-03 09:16:11 +0400

    commit

--- SOURCES/SCRIPTS/JS/lb_copy.js
+++ SOURCES/SCRIPTS/JS/lb_copy.js
@@ -8,7 +8,7 @@ $(function()
     $temp.remove();
   }
 
-  var array=["#LBPaper", "#LBVanilla", "#LB25w31a", "#LBPaperTest", "#LBFabric", "#LBOrigins", "#LBVoid", "#LBReclaim", "#LBNewPaper", "#LBNewFabric", "#LBVelocity", "#LBSAMP"];
+  var array=["#LBPaper", "#LBVanilla", "#LB404", "#LBHUB", "#LBPaperTest", "#LBFabric", "#LBOrigins", "#LBVoid", "#LBReclaim", "#LBNewPaper", "#LBNewFabric", "#LBVelocity", "#LBSAMP"];
   array.forEach(function(elem) {
     $(elem).on("click", function() {
       copyToClipboard(elem);
