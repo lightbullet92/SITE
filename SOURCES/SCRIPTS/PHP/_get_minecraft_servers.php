8a113c4471b791a36e5a00efd7fcd4ff8ade64b7
lightbullet92 <naa_1992@mail.ru>
2025-10-25 15:24:55 +0400

    commit

--- SOURCES/SCRIPTS/PHP/_get_minecraft_servers.php
+++ SOURCES/SCRIPTS/PHP/_get_minecraft_servers.php
@@ -6,11 +6,13 @@
     echo "\t" . "\t" . "\t" . '<summary><div class="lb_content_news_title">' . 'Проект Light Bullet' . '</div></summary>' . PHP_EOL;
     echo "\t" . "\t" . "\t" . '<div class="lb_content">' . PHP_EOL;
     echo "\t" . "\t" . "\t" . "\t" . '<p><span class="red"><b>Внимание:</b></span> Для того, чтобы скопировать IP сервера, достаточно просто на него нажать.</p>' . PHP_EOL;
-    echo "\t" . "\t" . "\t" . "\t" . '<p>Большинство серверов доступны через <button id="LBVelocity">lightbullet.ru:25565</button>. Поддерживаются релизные версии клиентов, вплоть до версии 1.21.8.</p>' . PHP_EOL;
+    echo "\t" . "\t" . "\t" . "\t" . '<p>Большинство серверов доступны через <button id="LBVelocity">lightbullet.ru:25565</button>. Поддерживаются релизные версии клиентов, вплоть до версии 1.21.8. Более новые версии клиентов проверяйте самостоятельно.</p>' . PHP_EOL;
     echo "\t" . "\t" . "\t" . "\t" . '<p>Представляю вашему вниманию сервера Minecraft:</p>' . PHP_EOL;
     echo "\t" . "\t" . "\t" . "\t" . '<p><table></p>' . PHP_EOL;
-    echo "\t" . "\t" . "\t" . "\t" . '<p><tr class="center bold"><td>Сервер</td><td>IP адрес</td><td>Карта</td><td>Версия</td><td>Доступность</td></tr></p>' . PHP_EOL;
+    echo "\t" . "\t" . "\t" . "\t" . '<p><tr class="center bold"><td>Сервер</td><td>Способ доступа</td><td>IP адрес</td><td>Карта</td><td>Версия</td><td>Доступность</td></tr></p>' . PHP_EOL;
+    $mc_server = "";
     $mc_ip = "";
+    $mc_access = "";
     $mc_port = 0;
     $mc_exists_dynmap = false;
     $mc_exists_map = false;
@@ -18,18 +20,21 @@
     $mc_color = "black";
     foreach($mc as $index => $item)
     {
-        switch($index % 5)
+        switch($index % 6)
         {
             case 0:
-                echo "\t" . "\t" . "\t" . "\t" . '<p><tr class="center"><td class="left"><b>' . str_replace("#", "", trim($item)) . '</b></td><td><button id="' . str_replace("#", "", trim($item)) . '">';
+                $mc_server = trim($item);
                 break;
             case 1:
-                $mc_ip = trim($item);
+                $mc_access = trim($item);
                 break;
             case 2:
-                $mc_port = trim($item);
+                $mc_ip = trim($item);
                 break;
             case 3:
+                $mc_port = trim($item);
+                break;
+            case 4:
                 if(trim($item) != "none")
                 {
                     $mc_dynmap_port = trim($item);
@@ -46,8 +51,8 @@
                     $mc_exists_map = false;
                 }
                 break;
-            case 4:
-                echo $mc_ip . ':' . $mc_port . '</button></td><td>';
+            case 5:
+                echo "\t" . "\t" . "\t" . "\t" . '<p><tr class="center"><td class="left"><b>' . str_replace("#", "", $mc_server) . '</b></td>' . '<td>' . $mc_access . '</td><td><button id="' . str_replace("#", "", $mc_server) . '">' . $mc_ip . ':' . $mc_port . '</button></td><td>';
                 if($mc_exists_dynmap)
                     echo '(<a target="_blank" href="http://' . $mc_ip . ':' . $mc_dynmap_port . '">map</a>)';
                 else
